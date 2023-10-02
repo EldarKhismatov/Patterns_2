4b842ce12c468ae4c51a6663cdab9030edf093c1
Eldar <ekhismatov@list.ru>
2023-10-02 09:25:30 +0500

    Change Tests

--- src/test/java/data/RegistrationInfo.java
+++ src/test/java/data/RegistrationInfo.java
@@ -1,11 +1,9 @@
-
+package data;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
-public class TestUser {
+public class RegistrationInfo {
     private final String login;
     private final String password;
     private final String status;
